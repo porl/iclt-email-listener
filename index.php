b1ddcdf255a0e7ad47789ceecc3612c8a3ec58e3
porl <porl42@gmail.com>
2012-07-17 10:54:06 +1000

    attempt to set correct scale for galaxy nexus

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile.structure-1.1.1.min.css">
 <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+<meta name="viewport" content="target-densitydpi=medium-dpi, initial-scale=1, maximum-scale=1">
 
 </head>
 <body>
