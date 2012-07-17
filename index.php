016e09bfdb3d92e3200dc0f0b4db342bedb69d15
porl <porl42@gmail.com>
2012-07-17 10:46:51 +1000

    link to iclt css

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <meta lang="en">
 <title>ICLT Email Listener</title>
 
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+<link rel="stylesheet" href="css/themes/iclt/iclt.min.css">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile.structure-1.1.1.min.css">
 <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
 
