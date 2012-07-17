635ff00a0900fdec828c3efe074ba21fd2992532
porl <porl42@gmail.com>
2012-07-17 10:07:57 +1000

    add jquery mobile calls to index

--- src/index.php
+++ src/index.php
@@ -3,6 +3,11 @@
 <head>
 <meta lang="en">
 <title>ICLT Email Listener</title>
+
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+
 </head>
 <body>
 <p>blahblah</p>
