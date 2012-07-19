d6f0d87cee6cc6c334e2ffebc603c58e433e2cd0
porl <porl42@gmail.com>
2012-07-19 10:56:11 +1000

    added notes in js for future

--- js/iclt.js
+++ js/iclt.js
@@ -1,4 +1,9 @@
 $(function() {
-	$("#ticketType input").click(function(){alert("hello")});
+
+	//on radio selection, close current container and change header to match
+
+	//on ticket type selection, reload results for services
+	
+	//on service selection, reload results for categories (filter by ticket type)
 
 });
