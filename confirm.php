52b52c772fe638e740c7a177298e941c81270650
porl <porl42@gmail.com>
2012-07-31 11:14:12 +1000

    finished ticket process - no email gen yet

---
+++ confirm.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+	case 'POST':
+		rest_post();
+		break;
+	case 'GET':
+		rest_get();
+		break;
+}
+
+function rest_get() {
+	//redirect to home
+	header( 'Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ) ;
+}
+
+function rest_post() {
+	require_once("inc/twig.php");
+	require_once("inc/data.php");
+	
+	$twigVars["root"] = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+	
+	$twigVars["post"] = $_POST;
+	echo $twig->render("confirm.html.twig", $twigVars);
+}
