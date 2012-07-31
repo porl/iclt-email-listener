12934c1f47ace655ca2ceb83aa7a0aab33278bee
porl <porl42@gmail.com>
2012-08-01 09:57:12 +1000

    begin enable email function for testing

--- submit.php
+++ submit.php
@@ -22,8 +22,36 @@ function rest_post() {
 	require_once("inc/twig.php");
 	require_once("inc/data.php");
 	
-$twigVars["root"] = rtrim("http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']), '/') . DIRECTORY_SEPARATOR;
+	require_once("inc/Swift/swift_required.php");
+	
+	$twigVars["root"] = rtrim("http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']), '/') . DIRECTORY_SEPARATOR;
 	
 	$twigVars["post"] = $_POST;
+	
+	
+	$body = $twig->render("email.twig", $twigVars);
+	$fieldTechEmail = $_POST['fieldTech'].'@dbb.org.au';
+	// Create the message
+	$message = Swift_Message::newInstance()
+	
+	// Give the message a subject
+	->setSubject('##ICLT Automatic Ticket Generation##')
+	
+	// Set the From address with an associative array
+	->setFrom($fieldTechEmail)
+	
+	//CC self
+	->setCc($fieldTechEmail)
+	
+	// Set the To addresses with an associative array
+	->setTo('porl42@gmail.com')
+	
+	// Give it a body
+	->setBody($body, 'text/plaintext')
+
+	;
+	
+	
+	
 	echo $twig->render("submit.html.twig", $twigVars);
 }
